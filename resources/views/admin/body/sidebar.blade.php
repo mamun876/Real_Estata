a22d2324526e1e3257e91dacfef37574a5716773
Mamun876 <mamunrrashid876@gmail.com>
2024-02-21 22:57:21 +0600

    agent add propert added from backend

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,3 +1,4 @@
+
 <nav class="sidebar">
     <div class="sidebar-header">
       <a href="#" class="sidebar-brand">
@@ -50,7 +51,7 @@
                    <a href="{{ route('all.amenitie') }}" class="nav-link">All Amenitie</a>
                  </li>
                  <li class="nav-item">
-                   <a href="pages/email/read.html" class="nav-link">Add Amenitie</a>
+                   <a href="{{ route('add.amenitie') }}" class="nav-link">Add Amenitie</a>
                  </li>
 
                </ul>
@@ -76,48 +77,22 @@
           </li>
            
       
-        <li class="nav-item nav-category">Components</li>
-        <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button" aria-expanded="false" aria-controls="uiComponents">
-            <i class="link-icon" data-feather="feather"></i>
-            <span class="link-title">UI Kit</span>
-            <i class="link-arrow" data-feather="chevron-down"></i>
-          </a>
-          <div class="collapse" id="uiComponents">
-            <ul class="nav sub-menu">
-              <li class="nav-item">
-                <a href="pages/ui-components/accordion.html" class="nav-link">Accordion</a>
-              </li>
-             
-            </ul>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="collapse" href="#advancedUI" role="button" aria-expanded="false" aria-controls="advancedUI">
-            <i class="link-icon" data-feather="anchor"></i>
-            <span class="link-title">Advanced UI</span>
-            <i class="link-arrow" data-feather="chevron-down"></i>
-          </a>
-          <div class="collapse" id="advancedUI">
-            <ul class="nav sub-menu">
-              <li class="nav-item">
-                <a href="pages/advanced-ui/cropper.html" class="nav-link">Cropper</a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/advanced-ui/owl-carousel.html" class="nav-link">Owl carousel</a>
-              </li>
-              
-            </ul>
-          </div>
-        </li>
-      
-        <li class="nav-item nav-category">Docs</li>
-        <li class="nav-item">
-          <a href="https://www.nobleui.com/html/documentation/docs.html" target="_blank" class="nav-link">
-            <i class="link-icon" data-feather="hash"></i>
-            <span class="link-title">Documentation</span>
-          </a>
-        </li>
+       
+          <li class="nav-item nav-category">User All Function</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button" aria-expanded="false" aria-controls="uiComponents">
+              <i class="link-icon" data-feather="feather"></i>
+              <span class="link-title">Manage Agent </span>
+              <i class="link-arrow" data-feather="chevron-down"></i>
+            </a>
+            <div class="collapse" id="uiComponents">
+              <ul class="nav sub-menu">
+                <li class="nav-item">
+                  <a href="{{ route('all.agent') }}" class="nav-link">All Agent </a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('add.agent') }}" class="nav-link">Add Agent</a>
+                </li>
       </ul>
     </div>
   </nav>
